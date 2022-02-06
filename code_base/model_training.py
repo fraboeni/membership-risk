d047d8519b515ff165657ef9b576a92691faa6d7
fraboeni <franziska.boenisch.fb@gmail.com>
2022-02-06 12:45:56 -0500

    extended model training to load data from disk (matches order of MI-Lira)

--- code_base/model_training.py
+++ code_base/model_training.py
@@ -1,17 +1,37 @@
 import os
 import tensorflow as tf
+import numpy as np
 
+from sklearn.preprocessing import minmax_scale
 from code_base.get_data import get_data
-from code_base.models import MODELS
+from code_base.models import *
+from keras.regularizers import l2
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 
-def main(dataset='cifar10', model_name='cifar10_cnn', augment=True, batch_size=64, lr=0.001, optim="Adam",
+def main(dataset='cifar10', model_name='cifar10_resnet50', augment=False, batch_size=64, lr=0.001, optim="Adam",
          momentum=0.9, nesterov=False, epochs=50, early_stop=True, save_model=True, log_training=True,
-         logdir='log_dir/models/', from_logits=True):
+         logdir='log_dir/models/', from_logits=True, kernel_regularizer=l2(0.0001), bias_regularizer=l2(0.0001),
+         data_dir='log_dir/result_data/experiments_64-20220204T144927Z-001/experiments_64'):
 
-    train_data, test_data = get_data(dataset, augmentation=augment, batch_size=batch_size, indices_to_use=range(0,25000))
+    # when data dir is not specified, we use the keras dataset, otherwise, we load data from disk
+    if data_dir is None:
+        train_data, test_data = get_data(dataset, augmentation=augment, batch_size=batch_size, indices_to_use=range(0,25000))
+    else:
+        load_location = os.getcwd() + '/../' + data_dir
+        x = np.load(load_location + "/x_train.npy")
+        x_scaled = minmax_scale(x.flatten(), feature_range=(0, 1)).reshape(50000, 32, 32, 3) # scale to range(0,1)
+        y = np.load(load_location + "/y_train.npy")
+
+        # todo: always chose different samples
+        # keep = np.random.uniform(0, 1, size=())
+        train_data = tf.data.Dataset.from_tensor_slices((x_scaled[:25000], y[:25000]))
+        test_data = tf.data.Dataset.from_tensor_slices((x_scaled[25000:], y[25000:]))
+
+        train_data = train_data.shuffle(25000).batch(batch_size)
+        test_data = test_data.batch(batch_size)
+
 
     if optim == "SGD":
         optimizer = tf.keras.optimizers.SGD(learning_rate=lr,
@@ -24,17 +44,21 @@ def main(dataset='cifar10', model_name='cifar10_cnn', augment=True, batch_size=6
         loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
     else:
         loss = tf.keras.losses.SparseCategoricalCrossentropy()
-    model = MODELS[model_name](from_logits=from_logits)
+    model = cifar10_cnn(from_logits=from_logits)
+
+    if kernel_regularizer is not None:
+        model = add_weight_decay(model, kernel_regularizer, bias_regularizer)
+
     model.compile(optimizer=optimizer,
                   loss=loss,
                   metrics=['accuracy']
                   )
-    model_id = 1004 #Todo parse from config file
+    model_id = 1012 #Todo parse from config file
 
     callbacks = []
 
     if early_stop:
-        early_stop_callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=3)
+        early_stop_callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=5)
         callbacks.append(early_stop_callback)
 
     if log_training:
@@ -43,11 +67,19 @@ def main(dataset='cifar10', model_name='cifar10_cnn', augment=True, batch_size=6
         logging_callback = tf.keras.callbacks.CSVLogger(logfile, separator=",", append=False)
         callbacks.append(logging_callback)
 
+    # if data_dir is None:
     history = model.fit(train_data,
                         validation_data=test_data,
                         epochs=epochs,
                         callbacks=callbacks,
                         )
+    # else:
+    #     history = model.fit(x=train_data[0],
+    #                         y=train_data[1],
+    #                         validation_data=test_data,
+    #                         epochs=epochs,
+    #                         callbacks=callbacks,
+    #                         )
     print(history.history)
 
     if save_model:
